e36330c9b7bb1bdc885988d31bdcac93f02ca262
Maharshi Patel <maharshi365@gmail.com>
2026-04-20 20:26:33 -0400

    moved db

--- src/db/drizzle.config.ts
+++ src/db/drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   out: "./drizzle",
   dialect: "sqlite",
   dbCredentials: {
-    url: "./data/sqlite.db",
+    url: "./meta/sqlite.db",
   },
 });
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
 import { Database } from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 
-const sqlite = new Database("data/sqlite.db");
+const sqlite = new Database("meta/sqlite.db");
 
 export const db = drizzle(sqlite);
